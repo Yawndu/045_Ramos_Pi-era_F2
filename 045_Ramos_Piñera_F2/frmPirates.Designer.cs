0eab65b952224ab5a2d6cbf93ac05b9fe43af680
Kyle <Kyle@LAPTOP-9AICKMGB>
2024-12-16 20:17:01 +0800

    Add project files.

---
+++ 045_Ramos_Piñera_F2/frmPirates.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace _045_Ramos_Piñera_F2
+{
+    partial class frmPirates
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.cboPirate = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.grdView = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAlias = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtBounty = new System.Windows.Forms.TextBox();
+            this.cboGroup = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(56, 58);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(233, 20);
+            this.txtKeyword.TabIndex = 0;
+            // 
+            // cboPirate
+            // 
+            this.cboPirate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPirate.FormattingEnabled = true;
+            this.cboPirate.Location = new System.Drawing.Point(318, 58);
+            this.cboPirate.Name = "cboPirate";
+            this.cboPirate.Size = new System.Drawing.Size(121, 21);
+            this.cboPirate.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(469, 53);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(104, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search Pirate";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // grdView
+            // 
+            this.grdView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdView.Location = new System.Drawing.Point(56, 134);
+            this.grdView.Name = "grdView";
+            this.grdView.ReadOnly = true;
+            this.grdView.RowHeadersVisible = false;
+            this.grdView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdView.Size = new System.Drawing.Size(589, 299);
+            this.grdView.TabIndex = 3;
+            this.grdView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdView_CellClick);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(77, 490);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(89, 46);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "View Details";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(256, 490);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(89, 46);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete Pirate Record";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNew);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboGroup);
+            this.groupBox1.Controls.Add(this.txtBounty);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtAlias);
+            this.groupBox1.Location = new System.Drawing.Point(664, 125);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 356);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pirate Information";
+            // 
+            // txtAlias
+            // 
+            this.txtAlias.Enabled = false;
+            this.txtAlias.Location = new System.Drawing.Point(94, 64);
+            this.txtAlias.Name = "txtAlias";
+            this.txtAlias.Size = new System.Drawing.Size(200, 20);
+            this.txtAlias.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(94, 110);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Enabled = false;
+            this.txtAge.Location = new System.Drawing.Point(94, 160);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(200, 20);
+            this.txtAge.TabIndex = 9;
+            // 
+            // txtBounty
+            // 
+            this.txtBounty.Enabled = false;
+            this.txtBounty.Location = new System.Drawing.Point(94, 245);
+            this.txtBounty.Name = "txtBounty";
+            this.txtBounty.Size = new System.Drawing.Size(200, 20);
+            this.txtBounty.TabIndex = 10;
+            // 
+            // cboGroup
+            // 
+            this.cboGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboGroup.Enabled = false;
+            this.cboGroup.FormattingEnabled = true;
+            this.cboGroup.Location = new System.Drawing.Point(94, 202);
+            this.cboGroup.Name = "cboGroup";
+            this.cboGroup.Size = new System.Drawing.Size(200, 21);
+            this.cboGroup.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Alias";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Pirate Group";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Bounty";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(37, 295);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(67, 42);
+            this.btnNew.TabIndex = 7;
+            this.btnNew.Text = "New Record";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(132, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(67, 42);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(227, 295);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(67, 42);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmPirates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 591);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.grdView);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboPirate);
+            this.Controls.Add(this.txtKeyword);
+            this.Name = "frmPirates";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmPirates";
+            this.Load += new System.EventHandler(this.frmPirates_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ComboBox cboPirate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView grdView;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboGroup;
+        private System.Windows.Forms.TextBox txtBounty;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAlias;
+    }
+}
+
---
+++ 045_Ramos_Piñera_F2/frmPirates.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _045_Ramos_Piñera_F2
+{
+    public partial class frmPirates : Form
+    {
+        string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Kyle\\Downloads\\dpPirates (1).accdb ";
+        OleDbConnection conn;
+        bool isnewrec;
+        bool isFirstButtonClick = false;
+        public frmPirates()
+        {
+            InitializeComponent();
+        }
+        private void refresh()
+        {
+
+            DataTable dt = new DataTable();
+            string query = "Select ID as ID, piratename as ALIAS, givenname as NAME, age as AGE, bounty as BOUNTY, pirategroup as PIRATEGROUP from pirates";
+            conn = new OleDbConnection(connStr);
+            conn.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+
+
+
+            grdView.DataSource = dt;
+            grdView.Columns["age"].Visible = false;
+            grdView.Columns["ID"].Visible = false;
+        }
+        private void distinct()
+        {
+            DataTable dt = new DataTable();
+            string query = "select distinct pirategroup from pirates";
+            conn = new OleDbConnection(connStr);
+            conn.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+
+            grdView.DataSource = dt;
+
+            cboPirate.DataSource = dt;
+            cboPirate.DisplayMember = "pirategroup";
+            cboGroup.DataSource = dt;
+            cboGroup.DisplayMember = "pirategroup";
+        }
+
+        private void frmPirates_Load(object sender, EventArgs e)
+        {
+            refresh();
+
+            distinct();
+
+            DataTable dt = new DataTable();
+            string query = "Select ID as ID, piratename as ALIAS, givenname as NAME, age as AGE, bounty as BOUNTY, pirategroup as PIRATEGROUP from pirates";
+            conn = new OleDbConnection(connStr);
+            conn.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+
+            grdView.DataSource = dt;
+
+            grdView.Columns["age"].Visible = false;
+            grdView.Columns["ID"].Visible = false;
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtKeyword.Text) || (string.IsNullOrEmpty(cboPirate.Text)))
+            {
+                MessageBox.Show("Please input correct data", "ERROR");
+            }
+            else
+            {
+                DataTable dt = new DataTable();
+                string query = "Select piratename as ALIAS, givenname as NAME, age as AGE, bounty as BOUNTY, pirategroup as PIRATEGROUP from pirates where piratename = '" + txtKeyword.Text + "' or givenname = '" + txtKeyword.Text + "'";
+                conn = new OleDbConnection(connStr);
+                conn.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+                adapter.Fill(dt);
+                conn.Close();
+
+                grdView.DataSource = dt;
+            }
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            isnewrec = false;
+            btnSave.Enabled = true;
+            btnNew.Enabled = false;
+            txtAlias.Enabled = true;
+            txtName.Enabled = true;
+            txtAge.Enabled = true;
+            cboGroup.Enabled = true;
+            txtBounty.Enabled = true;
+            DataTable dt = new DataTable();
+            string query = "Select piratename as ALIAS, givenname as NAME, age as AGE, pirategroup as PIRATEGROUP, bounty as BOUNTY from pirates where piratename = '" + txtAlias.Text + "'";
+            conn = new OleDbConnection(connStr);
+            conn.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+
+            grdView.DataSource = dt;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (isnewrec == true)
+            {
+                string query = "INSERT into [pirates] (piratename, givenname, age, bounty, pirategroup) values(@alias, @name, @age, @bounty, @pirategroup)";
+                conn = new OleDbConnection(connStr);
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@alias", txtAlias.Text);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@age", txtAge.Text);
+                cmd.Parameters.AddWithValue("@bounty", txtBounty.Text);
+                cmd.Parameters.AddWithValue("@pirategroup", cboGroup.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Input added");
+
+                refresh();
+            }
+
+            else
+            {
+
+                string query = "Update [pirates] set piratename = @alias, givenname = @name, age = @age, bounty = @bounty, pirategroup = @pirategroup where ID = @id";
+                conn = new OleDbConnection(connStr);
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@alias", txtAlias.Text);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@age", txtAge.Text);
+                cmd.Parameters.AddWithValue("@bounty", txtBounty.Text);
+                cmd.Parameters.AddWithValue("@pirategroup", cboGroup.Text);
+                cmd.Parameters.AddWithValue("@id", grdView.SelectedCells[0].Value.ToString());
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (isFirstButtonClick)
+                {
+                    MessageBox.Show("Saved");
+                }
+                else
+                {
+                    isFirstButtonClick = true;
+                    MessageBox.Show("Click the save button again to save!");
+                }
+
+
+
+                refresh();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            isnewrec = true;
+            txtAlias.Text = "";
+            txtName.Text = "";
+            txtAge.Text = "";
+            cboGroup.Text = "";
+            txtBounty.Text = "";
+            btnSave.Enabled = true;
+
+
+            txtAlias.Enabled = true;
+            txtName.Enabled = true;
+            txtAge.Enabled = true;
+            cboGroup.Enabled = true;
+            txtBounty.Enabled = true;
+            btnNew.Enabled = true;
+        }
+
+        private void grdView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtAlias.Text = grdView.SelectedCells[1].Value.ToString();
+            txtName.Text = grdView.SelectedCells[2].Value.ToString();
+            txtAge.Text = grdView.SelectedCells[3].Value.ToString();
+            txtBounty.Text = grdView.SelectedCells[4].Value.ToString();
+            cboGroup.Text = grdView.SelectedCells[5].Value.ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtAlias.Text = "";
+            txtName.Text = "";
+            txtAge.Text = "";
+            cboGroup.Text = "";
+            txtBounty.Text = "";
+
+
+            txtAlias.Enabled = false;
+            txtName.Enabled = false;
+            txtAge.Enabled = false;
+            cboGroup.Enabled = false;
+            txtBounty.Enabled = false;
+            btnSave.Enabled = false;
+            btnNew.Enabled = true;
+
+            refresh();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "Delete from pirates where piratename = @alias";
+            conn = new OleDbConnection(connStr);
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            cmd.Parameters.AddWithValue("@alias", txtAlias.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Data Deleted");
+
+            txtAlias.Text = "";
+            txtName.Text = "";
+            txtAge.Text = "";
+            cboGroup.Text = "";
+            txtBounty.Text = "";
+
+            refresh();
+        }
+    }
+}
